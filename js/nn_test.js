c43bae1ebf2f6c9149541f813a8733abd4ef8d79
yetdarkerdevelopment <yetdarkerdevelopment@gmail.com>
2023-08-12 12:42:06 +0200

    More testing.

--- js/nn_test.js
+++ js/nn_test.js
@@ -1,7 +1,7 @@
 import { dotProduct } from "./neuralnetwork.js";
 
-let mA = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-let mB = [[10, 11, 12], [13, 14, 15], [16, 17, 18]];
+let mA = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]];
+let mB = [[13, 14, 15, 16], [17, 18, 19, 20], [21, 22, 23, 24]];
 
 console.log(dotProduct(mA, mB));
 
